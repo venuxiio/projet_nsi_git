48dcd2666334c1335137838a62dd71571d487e51
srichez888 <srichez888@gmail.com>
2021-12-12 20:04:30 +0100

    test

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ pygame.init()
 # creation de la fenetre
 pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Find Eve")
-
+print()
 running = True
 while running:
     for event in pygame.event.get():
