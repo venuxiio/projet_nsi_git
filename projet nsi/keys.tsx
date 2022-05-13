38bd291f9bf322a112e56e5cea67fc19b271d953
venuxiio <95481660+venuxiio@users.noreply.github.com>
2022-05-13 13:30:17 +0200

    Delete projet nsi directory

--- projet nsi/keys.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="keys" tilewidth="32" tileheight="32" tilecount="16" columns="4">
- <image source="keys.png" trans="000000" width="128" height="128"/>
-</tileset>
