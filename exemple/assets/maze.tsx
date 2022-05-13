337c4ffbe495e596f138facfa6300c6f11bf4f2e
venuxiio <95481660+venuxiio@users.noreply.github.com>
2022-05-13 13:21:36 +0200

    Delete exemple directory

--- exemple/assets/maze.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="maze" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="maze.png" trans="ffffff" width="256" height="256"/>
-</tileset>
--- exemple/assets/maze2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="maze2" tilewidth="32" tileheight="32" tilecount="30" columns="5">
- <image source="maze2.png" width="160" height="192"/>
-</tileset>
